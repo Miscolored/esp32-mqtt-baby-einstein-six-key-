9d89d6671c6bd5ebec59eebd22c80dc4c22eb1ce
miscolored <miscolored@gmail.com>
2023-03-14 19:10:08 -0500

    as deployed

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,28 @@
 #include <Arduino.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
-#include <WebServer.h>
+//#include <WebServer.h>
 
 #include "baby_einstein_six_key.h"
 #include "mqtt.h"
 #include "secret.h"
+
+#define LED_PIN 8
 
 WiFiClient wifi_client;
 PubSubClient mqtt_client(wifi_client);
 
 volatile uint8_t color_pin = 0; // GPIO pin of color pressed or 0 otherwise
 
+void disableWiFi() {
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+}
+
 void interrupt_keyboard_press(void* arg) {
-  color_pin = *((int*) arg);
+  if (color_pin == 0) {
+    color_pin = *((int*) arg);
+  }
 }
 
 void mqqt_color_pin_publish(int color_pin) {
@@ -28,15 +37,16 @@ void mqqt_color_pin_publish(int color_pin) {
 
 void setup() {
   Serial.begin(9600);
-  while(!Serial.available()) {}
+  
   Serial.println("Booting");
 
-  pinMode(RED_PIN, INPUT);
-  pinMode(ORANGE_PIN, INPUT);
-  pinMode(YELLOW_PIN, INPUT);
-  pinMode(GREEN_PIN, INPUT);
-  pinMode(BLUE_PIN, INPUT);
-  pinMode(PURPLE_PIN, INPUT);
+  pinMode(RED_PIN, INPUT_PULLDOWN);
+  pinMode(ORANGE_PIN, INPUT_PULLDOWN);
+  pinMode(YELLOW_PIN, INPUT_PULLDOWN);
+  pinMode(GREEN_PIN, INPUT_PULLDOWN);
+  pinMode(BLUE_PIN, INPUT_PULLDOWN);
+  pinMode(PURPLE_PIN, INPUT_PULLDOWN);
+  pinMode(LED_PIN, OUTPUT);
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -48,6 +58,9 @@ void setup() {
     delay(5000);
     ESP.restart();
   }
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
   mqtt_client.setServer(MQTT_BROKER, MQTT_PORT);
   mqtt_client.setBufferSize(255);
@@ -65,31 +78,28 @@ void setup() {
   attachInterruptArg(BLUE_PIN, interrupt_keyboard_press, (void*) &BLUE_PIN, RISING);
   attachInterruptArg(PURPLE_PIN, interrupt_keyboard_press, (void*) &PURPLE_PIN, RISING);
 
-
   Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  
+  mqtt_client.publish(BABY_EINSTEIN_SIX_KEY_TOPIC, "esp32 setup() completed.");
 }
 
 
 void loop() {
-
+  digitalWrite(LED_PIN, HIGH);
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("no wifi");
-    delay(500);
+    digitalWrite(LED_PIN, LOW);
   }
 
   if (!mqtt_client.connected() and !mqtt_client.connect(BABY_EINSTEIN_SIX_KEY_USER, BABY_EINSTEIN_SIX_KEY_USER, BABY_EINSTEIN_SIX_KEY_PASSWORD)) {
     Serial.print("no mqqt ");
     Serial.println(mqtt_client.state());
-    delay(500);
+    digitalWrite(LED_PIN, LOW);
   }
 
   if (color_pin > 0) {
     mqqt_color_pin_publish(color_pin);
     color_pin = 0;
-    delay(500);
   }
+  delay(500);
   yield();
 }
